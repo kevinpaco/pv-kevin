449d3a9c9e94799710ff53b8ca02e56f05620a49
kevinpaco <kevinpaco40@gmail.com>
2022-05-31 11:05:50 -0300

    inicio tp5

---
+++ kevin-tp5/src/test/java/ar/edu/unju/fi/pvisual/KevinTp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinTp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
